8455a3f4c5f965cbd1e79b816789a2a39d508154
ferdous85 <ferdous85@gmail.com>
2021-05-31 20:26:28 +0600

    css cng

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 
 function App() {
-  
+  const categories=[
+      `Women's Running Shoes`,
+        `Everyday Sneakers`,
+        `Slio-Ons`,
+        `High Tops`,
+        `Boat Shoes`,
+        `Flats`,
+        `Weather Repellent Shoes`
+  ]
 
   return (
     <div >
@@ -12,7 +20,8 @@ function App() {
       <a href="/" className="underline">Learn More</a>
     </p>
      </div>
-     <header className="relative flex px-4 py-3 items-center justify-between bg-white shadow-lg">
+     <header className="relative flex px-5 py-3 items-center justify-between bg-white shadow-lg">
+       <div className='absolute inset-0 shadow-lg opacity-50'></div>
        <div className='flex'>
          <button className='h-8 w-8'>
          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -32,6 +41,34 @@ function App() {
          </button>
        </div>
      </header>
+     <main>
+       <div className='flex justify-between item-center px-8 py-4 pt-4 pb-3'>
+        <div>
+        <div className='text-xs font-medium text-gray-900 space-x-1'>
+          <a href="/" className='underline' >Home</a>
+          <span>/</span>
+        </div>
+        <div className='text-lg font-bold text-gray-900'>
+          Women's Shoes
+        </div>
+        </div>
+        <div className='flex'>
+          <button className='h-6 w-6'>
+          <svg xmlns="http://www.w3.org/2000/svg" className="transform rotate-90 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+          </svg>
+          </button>
+        </div>
+       </div>
+       <div>
+       <div className='bg-gray-100 pt-6' >
+        {categories.map((category)=>(
+          <a href="/"> {category} </a>
+        ))}
+       </div>
+       </div>
+     </main>
+
     </div>
   );
 }
